99bee4b63f4ca9ac863b0e9c77f7a950fdef067b
cmangkuo <cm6652@gmail.com>
2021-03-27 20:14:04 -0700

    Deleted some unused variables

--- img2RGB.py
+++ img2RGB.py
@@ -7,16 +7,11 @@ def img2RGB(x, y):
 	img = Image.open(file)
 	img = img.resize((x,y))
 	pixels = list(img.getdata())
-	img.show()
-	
-	row = []
-	
 	return pixels
 	
 	
 if __name__ == "__main__":
 	matrix = img2RGB(800,600)
-	
 	index = 0
 	for x in range(800):
 		row = []
