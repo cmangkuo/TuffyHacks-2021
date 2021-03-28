c452236749e107079f82adb25c1ee6e6701d6d7d
cmangkuo <cm6652@gmail.com>
2021-03-27 19:59:34 -0700

    Image to RGB data conversion

---
+++ img2RGB.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import easygui
+import numpy as np
+
+def img2RGB(x, y):
+	## Load image
+	file = easygui.fileopenbox()
+	img = Image.open(file)
+	img = img.resize((x,y))
+	pixels = list(img.getdata())
+	img.show()
+	
+	row = []
+	
+	return pixels
+	
+	
+if __name__ == "__main__":
+	matrix = img2RGB(16,9)
+	
+	index = 0
+	for x in range(16):
+		row = []
+		for y in range(9):
+			row.append(matrix[index])
+			index += 1
+		print(row)
